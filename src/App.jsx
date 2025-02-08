3ef444a6c29820221c3ba1228079f4b58138f505
PPOSM1993 <pedro.osorio04@inacapmail.cl>
2025-02-08 14:16:41 -0300

    Creacion Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 
+
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
+import Display from './components/Display'
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar/>
+        <Display/>
       </div>
       <Player/>
     </div>
