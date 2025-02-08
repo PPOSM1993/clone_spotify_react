eb3e02aa5b2355922efb843c092890d26fbf0133
PPOSM1993 <pedro.osorio04@inacapmail.cl>
2025-02-07 22:18:53 -0300

    creacion component Player y finalizacion Sidebar

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -82,14 +82,14 @@ export const albumsData = [
     },
     {   
         id:1,
-        name: "Top 50 India",
+        name: "Top 50 Chile",
         image: img9,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#22543d"
     },
     {   
         id:2,
-        name: "Trending India",
+        name: "Trending Chile",
         image: img10,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#742a2a"
